3717aff4b8529d044d795018195deb7eb00b8cfb
Vitalson Lorran Viana Carvalhal <vitalson_lorran95@ymail.com>
2021-09-21 22:33:57 -0300

    First Commit

---
+++ Transicao de Tela/Transicao de Tela/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Transicao de Tela
+//
+//  Created by Vitalson Lorran on 21/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Transicao de Tela/Transicao de TelaUITests/Transicao_de_TelaUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Transicao_de_TelaUITestsLaunchTests.swift
+//  Transicao de TelaUITests
+//
+//  Created by Vitalson Lorran on 21/09/21.
+//
+
+import XCTest
+
+class Transicao_de_TelaUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
